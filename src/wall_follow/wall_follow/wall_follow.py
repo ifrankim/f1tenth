c08d0e7a88dc27804649b27563565c7c357a42c7
Francisco <fkgaieski@gmail.com>
2024-02-02 15:29:01 +0800

    feat(wall_foloow): added steering_angle_control and speed_control

--- src/wall_follow/wall_follow/wall_follow.py
+++ src/wall_follow/wall_follow/wall_follow.py
@@ -27,20 +27,32 @@ class WallFollowNode(Node):
         self.publisher = self.create_publisher(AckermannDriveStamped, 'drive', 10)
 
     def scan_callback(self, msg):
-        collision_imminent = self.check_tts(msg)
+        steering_angle = self.__steering_angle_control__(msg)
+        speed = self.__speed_control__(steering_angle)
 
-        if collision_imminent:
-            drive_msg = AckermannDriveStamped()
-            drive_msg.drive.speed = 0.0 
-            self.publisher.publish(drive_msg)
+        drive_msg = AckermannDriveStamped()
+        drive_msg.drive.steering_angle = steering_angle
+        drive_msg.drive.speed = speed
+        self.publisher.publish(drive_msg)
 
     def odom_callback(self, msg):
         self.velocity_x = msg.twist.twist.linear.x
 
-    def check_tts(self, scan_data):
+    def __speed_control__(self, steering_angle):
+        speed = 0.0
+        if steering_angle >= 0 and steering_angle < 10:
+            speed = 1.5
+        elif steering_angle >= 10 and steering_angle < 20:
+            speed = 1.0
+        else:
+            speed = 0.5
+        return speed
+
+    def __steering_angle_control__(self, scan_data):
         ranges = scan_data.ranges
         angle_max = 2.3499999046325684
         angle_min = -angle_max
+        l = self.velocity_x*0.25
 
         a_range = 419
         b_range = 177
@@ -48,26 +60,13 @@ class WallFollowNode(Node):
         b_distance = range[b_range]
         # dt_distance = min(ranges)
         theta = (abs(angle_max)+abs(angle_min))/len(ranges) * (a_range - b_range)
-
         
-        alpha = 1/(math.tan((a_distance*math.cos(theta) - b_distance)/alpha*math.sin(theta)))
+        alpha = 1/(math.tan((a_distance*math.cos(theta) - b_distance)/a_distance*math.sin(theta)))
 
         dt_distance = b_distance*math.cos(alpha)
 
-        for i in range(len(ranges)):
-            current_range = ranges[i]
-            
-            theta = (abs(angle_max)+abs(angle_min))/len(ranges) * i
+        dt_distance_1 = dt_distance + l*math.sin(alpha)
 
-            if math.isnan(current_range) or self.velocity_x*math.cos(theta-angle_max) == 0:
-                continue
-            
-            ttc = current_range/((self.velocity_x)*math.cos(theta-angle_max))
-            if (ttc < 0.9 and ttc > 0):
-                self.get_logger().info(f"TTC: {ttc}")
-                return True
-
-        return False
 
 def main(args=None):
     rclpy.init(args=args)
