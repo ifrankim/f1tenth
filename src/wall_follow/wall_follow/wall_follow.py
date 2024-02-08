9aff1b0d1056cfcff41929cddafdb6b9fad2d18b
Francisco <fkgaieski@gmail.com>
2024-02-08 18:43:47 +0800

    feat(wall_follow): finished algo

--- src/wall_follow/wall_follow/wall_follow.py
+++ src/wall_follow/wall_follow/wall_follow.py
@@ -4,7 +4,6 @@ from rclpy.node import Node
 from ackermann_msgs.msg import AckermannDriveStamped
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import LaserScan
-import matplotlib.pyplot as plt
 import time
 
 
@@ -16,7 +15,8 @@ class WallFollowNode(Node):
         self.velocity_x = 0.0
         self.pid_i = 0.0
         self.previous_error = 0.0
-        self.last_callback_time = 0
+        self.last_callback_time = time.time()
+        self.prev_steering_angle = 0
 
         self.subscription = self.create_subscription(
             LaserScan, "scan", self.scan_callback, 10
@@ -26,25 +26,23 @@ class WallFollowNode(Node):
         )
         self.publisher = self.create_publisher(AckermannDriveStamped, "drive", 10)
 
-        self.points = []
+    def odom_callback(self, msg):
+        self.velocity_x = msg.twist.twist.linear.x
 
     def scan_callback(self, msg):
         if self.last_callback_time is not None:
-            if len(self.points) < 3000:
-                steering_angle = self.__steering_angle_control__(msg)
-                speed = self.__speed_control__(steering_angle)
-
-                drive_msg = AckermannDriveStamped()
-                drive_msg.drive.steering_angle = steering_angle
-                drive_msg.drive.speed = speed
-                self.publisher.publish(drive_msg)
-            else:
-                plt.plot(list(range(0, len(self.points), 1)), self.points)
-                plt.axhline(y=0.5, color="r", linestyle="--", label="Linha reta em y=5")
-                plt.show()
-
-    def odom_callback(self, msg):
-        self.velocity_x = msg.twist.twist.linear.x
+            steering_angle = self.__steering_angle_control__(msg)
+            speed = self.__speed_control__(steering_angle)
+            
+            # steering_angle = self.prev_steering_angle - steering_angle
+            self.prev_steering_angle = steering_angle
+            
+            # self.get_logger().info(f"angle: {steering_angle*180/math.pi:.3f}")
+            
+            drive_msg = AckermannDriveStamped()
+            drive_msg.drive.steering_angle = steering_angle
+            drive_msg.drive.speed = speed
+            self.publisher.publish(drive_msg)
 
     def __speed_control__(self, steering_angle):
         speed = 0.0
@@ -63,29 +61,34 @@ class WallFollowNode(Node):
     def __steering_angle_control__(self, scan_data):
         current_time = time.time()
         dt = current_time - self.last_callback_time
-        # print("Interval between callbacks:", interval)
         error = self.__get_error__(scan_data)
 
         steering_angle = self.__pid_control__(error, dt)
+        
+        self.get_logger().info(f"error:{error:.3f} angle: {steering_angle*180/math.pi:.3f}")
+        
         self.previous_error = error
         self.last_callback_time = current_time
 
-        self.get_logger().info(
-            f"error: {error:.3f}, angle: {steering_angle*180/math.pi:.3f}"
-        )
         return steering_angle
+
+    def __get_range_index__(self, scan_data, angle):
+        return int(
+            len(scan_data.ranges)
+            - (
+                (scan_data.angle_max - self.__to_radians__(angle))
+                / scan_data.angle_increment
+            )
+        )
 
     def __get_error__(self, scan_data):
         ranges = scan_data.ranges
         angle_increment = scan_data.angle_increment
 
-        l = self.velocity_x * 2
+        l = self.velocity_x * 0.2
 
-        a_range = 419
-        b_range = 177
-
-        # a_range = len(ranges) - 419
-        # b_range = len(ranges) - 177
+        a_range = self.__get_range_index__(scan_data, 45)
+        b_range = self.__get_range_index__(scan_data, 90)
 
         a_distance = ranges[a_range]
         b_distance = ranges[b_range]
@@ -97,38 +100,39 @@ class WallFollowNode(Node):
         )
 
         dt_distance = b_distance * math.cos(alpha)
-        dt_distance_1 = dt_distance + l * math.sin(alpha)
 
-        desired_distance = 0.5
+
+        # middle_index = len(ranges) // 2
+        # dt_distance = min(ranges[middle_index:])
+        # alpha = math.acos(dt_distance/b_distance)
+
+        desired_distance = 1.45
+        # self.get_logger().info(f"d:{dt_distance:.3f}")
+
+        dt_distance_1 = dt_distance + 12 * math.sin(alpha)
         error = desired_distance - dt_distance_1
-
-        self.points.append(dt_distance_1)
+        # if (dt_distance > desired_distance): error *= -1
+        self.get_logger().info(f"e:{error:.3f}, dt:{dt_distance:.3f} desr:{desired_distance:.3f}")
 
         return error
 
     def __pid_control__(self, error, dt):
-        kp = 3
+        kp = 1
         ki = 0
-        kd = 0.1
+        kd = 0.09
 
         pid_p = kp * error
-        pid_d = kd * (error - self.previous_error) / dt
-
-        if -3 < error and error < 3:
-            self.pid_i = (self.pid_i + error) * dt * ki
-        else:
-            self.pid_i = 0
+        pid_d = kd * (error - self.previous_error)
+        self.pid_i = (self.pid_i + error) * dt * ki
 
         steering_angle = pid_p + self.pid_i + pid_d
 
-        if steering_angle > 80 * math.pi / 180:
-            steering_angle = 80 * math.pi / 180
-        if steering_angle < -80 * math.pi / 180:
-            steering_angle = -80 * math.pi / 180
-
-        # self.get_logger().info(f"p:{pid_p:.3f} i: {self.pid_i:.3f}, d: {pid_d:.3f} angle: {steering_angle:.3f}")
-
-        return steering_angle
+        # if steering_angle > self.__to_radians__(90):
+        #     steering_angle = self.__to_radians__(90)
+        # if steering_angle < -self.__to_radians__(90):
+        #     steering_angle = -self.__to_radians__(90)
+        
+        return -steering_angle
 
     def __to_radians__(self, angle):
         return math.pi * angle / 180.0
